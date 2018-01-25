e4af255e9f1dac8ad14c9fe3e24d79faa1a2a86e
Nitin Bhadauria <nitin.bhadauria@tothenew.com>
2018-01-25 09:26:41 +0530

    Create entry_point.sh

---
+++ entry_point.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -o errexit
+set -o xtrace
+
+export EC2_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
+port="$(curl https://raw.githubusercontent.com/nitinbhadauria/rocketchat-aws-ecs/master/ecs-port-mapping.js | node)"
+eval "$port"
+node main.js
