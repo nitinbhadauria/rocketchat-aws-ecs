e2f3469404ce91027bb2ced4f3645e7cba89d200
bijoypaul <bijoy.paul@tothenew.com>
2018-02-01 20:45:32 +0530

    function for ecs-port-mapping added

--- app/bundle/programs/server/app/app.js
+++ app/bundle/programs/server/app/app.js
@@ -6451,24 +6451,8 @@ Meteor.startup(function () {
 	}
 
 	/* This script will read ECS_CONTAINER_METADATA_FILE file and set up dynamic port */
-	if (process.env.ECS_CONTAINER_METADATA_FILE) {
-	    // Define JSON File
-	    var fs = require("fs");
-	    // Get content from file ECS_CONTAINER_METADATA_FILE
-	    var contents = fs.readFileSync(process.env.ECS_CONTAINER_METADATA_FILE);
-	    // Define to JSON type
-	    var jsonContent = JSON.parse(contents);
-	    // Check the container start to be ready before we read the ports
-	    while (jsonContent.MetadataFileStatus !== 'READY') {
-	        // Get content from file ECS_CONTAINER_METADATA_FILE
-	            var contents = fs.readFileSync(process.env.ECS_CONTAINER_METADATA_FILE);
-	        // Define to JSON type
-	            var jsonContent = JSON.parse(contents);
-	            console.log('Re-try to fetch the status..');
-	    }
-	    // Set the Port
-	    instance.port = String(jsonContent.PortMappings[0].HostPort).trim();
-	};
+	changeInstanceDetail = require('./ecs-port-mapping.js')
+	changeInstanceDetail(instance);
 
 	InstanceStatus.registerInstance('rocket.chat', instance);
 	UserPresence.start();
